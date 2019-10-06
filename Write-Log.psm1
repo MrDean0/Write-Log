6333489f5aa224c1d07ebd330c6d567175d27148
MrDean0 <51010076+MrDean0@users.noreply.github.com>
2019-10-06 23:46:35 +0800

    minor changes to the way log file name is created
    file name will now be changed to a default value if not being called from a script

--- Write-Log.psm1
+++ Write-Log.psm1
@@ -1,15 +1,13 @@
-﻿<#
+<#
  .Synopsis
-  Writes a time stamped log to a given file
-
-
- .Parameter Piped content
+  wires logs to log folder at root of script
+ .Description 
+  writes log to folder created at root of script calling function if not called from script then log will be created at the current working directory
+ .Parameter pipe
   content to be writen to the log
-
- .Example
+ .Examples
    $Processes | write-log
    "blahblah" | Write-log
-
 #>
 function Write-log {
     Param(
@@ -18,18 +16,23 @@ function Write-log {
         [String[]]$Log
         )
     try {
+        if (($MyInvocation.ScriptName)){ 
+            #sets log file name to script name if being called from a script
+            $logFileName = "$([io.path]::GetFileNameWithoutExtension($MyInvocation.ScriptName)).log"
+        }else{
+            $logFileName = "Temp.log"
+        }
         $LogFolderName = "Logs"
         $logFolderPath = "$PSScriptRoot\" + "$LogFolderName\"
-        $logFileName = "$([io.path]::GetFileNameWithoutExtension($PSCommandPath)).log"
         $LogFilePath = $logFolderPath + $logFileName
-  
-        if (!(Test-path $logFolderPath)){ new-Item -Path $logFolderPath -ItemType directory }
-        if (!(Test-path $LogFilePath)){ New-Item -Path $LogFilePath -ItemType File }
-        
+        #creates directorys if needed
+        if (!(Test-path $logFolderPath)){ new-Item -Path $logFolderPath -ItemType directory | Out-Null}
+        if (!(Test-path $LogFilePath)){ New-Item -Path $LogFilePath -ItemType File | Out-Null}        
         $LogContent = Get-Content $LogFilePath
         $date = Get-Date -Format "d/M/yyyy h:mm:ss tt"
         $Out = $date + " - " +  $Log
         Set-Content $LogFilePath -Value $Out,$LogContent
+
     }catch{
 
     Write-Output "Something broke here is the error "
@@ -37,8 +40,3 @@ function Write-log {
     Write-Output $_.ScriptStackTrace
     }
 }
-
-
-  
-        
-  
